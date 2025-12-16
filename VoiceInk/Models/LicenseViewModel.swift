65cd72027ff4b474cbe61790792a9144d2f3262e
Zané <36542885+zane-commeatio@users.noreply.github.com>
2025-12-16 10:14:45 +0700

    Make it free forever
    Removed license key validation and trial checks, setting license state to licensed by default.

--- VoiceInk/Models/LicenseViewModel.swift
+++ VoiceInk/Models/LicenseViewModel.swift
@@ -33,41 +33,9 @@ class LicenseViewModel: ObservableObject {
     }
     
     private func loadLicenseState() {
-        // Check for existing license key
-        if let licenseKey = userDefaults.licenseKey {
-            self.licenseKey = licenseKey
-            
-            // If we have a license key, trust that it's licensed
-            // Skip server validation on startup
-            if userDefaults.activationId != nil || !userDefaults.bool(forKey: "VoiceInkLicenseRequiresActivation") {
-                licenseState = .licensed
-                activationsLimit = userDefaults.activationsLimit
-                return
-            }
-        }
-        
-        // Check if this is first launch
-        let hasLaunchedBefore = userDefaults.bool(forKey: "VoiceInkHasLaunchedBefore")
-        if !hasLaunchedBefore {
-            // First launch - start trial automatically
-            userDefaults.set(true, forKey: "VoiceInkHasLaunchedBefore")
-            startTrial()
-            return
-        }
-        
-        // Only check trial if not licensed and not first launch
-        if let trialStartDate = userDefaults.trialStartDate {
-            let daysSinceTrialStart = Calendar.current.dateComponents([.day], from: trialStartDate, to: Date()).day ?? 0
-            
-            if daysSinceTrialStart >= trialPeriodDays {
-                licenseState = .trialExpired
-            } else {
-                licenseState = .trial(daysRemaining: trialPeriodDays - daysSinceTrialStart)
-            }
-        } else {
-            // No trial has been started yet - start it now
-            startTrial()
-        }
+        // Always appear licensed and never check activation
+        licenseState = .licensed
+        activationsLimit = 0  // Or any unlimited value
     }
     
     var canUseApp: Bool {
